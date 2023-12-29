25c0d6884e5cec17caf6b3f5d56ee30941ae4e80
aliammari <ali.ammari@esprit.tn>
2023-12-29 03:50:50 +0100

    changing css to tailwindcss

--- src/RepoTrafficViewer.js
+++ src/RepoTrafficViewer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Octokit } from "octokit";
-import "./RepoTrafficViewer.css";
+import "./index.css";
 
 const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN,
@@ -58,67 +58,78 @@ function RepoTrafficViewer() {
 
   return (
     <section id="repo-traffic">
-      <h2>Repo Traffic Viewer</h2>
-      <div className="container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>URL</th>
-              <th>Views</th>
-              <th>Unique Views</th>
-              <th>Clones</th>
-              <th>Unique Clones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {repos.map(
-              ({
-                name,
-                url,
-                views,
-                uniqueViews,
-                clones,
-                uniqueClones,
-                yesterdayViews,
-                yesterdayUniqueViews,
-                yesterdayClones,
-                yesterdayUniqueClones,
-              }) => {
-                return (
-                  <tr key={name}>
-                    <td>{name}</td>
-                    <td>
-                      <a href={url} target="_blank" rel="noopener noreferrer">
-                        {url}
-                      </a>
-                    </td>
-                    <td className="dataItem">
-                      <strong className="today">{views}</strong>
-                      <strong className="yesterday">{yesterdayViews}</strong>
-                    </td>
-                    <td className="dataItem">
-                      <strong className="today">{uniqueViews}</strong>
-                      <strong className="yesterday">
-                        {yesterdayUniqueViews}
-                      </strong>
-                    </td>
-                    <td className="dataItem">
-                      <strong className="today">{clones}</strong>
-                      <strong className="yesterday">{yesterdayClones}</strong>
-                    </td>
-                    <td className="dataItem">
-                      <strong className="today">{uniqueClones}</strong>
-                      <strong className="yesterday">
-                        {yesterdayUniqueClones}
-                      </strong>
-                    </td>
-                  </tr>
-                );
-              }
-            )}
-          </tbody>
-        </table>
+      <h2 className="text-2xl font-bold mb-4">Repo Traffic Viewer</h2>
+      <div className="bg-gray-200 p-4 rounded-lg shadow-inner">
+        <div className="overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 bg-gray-300">Name</th>
+                <th className="py-2 px-4 bg-gray-300">URL</th>
+                <th className="py-2 px-4 bg-gray-300">Views</th>
+                <th className="py-2 px-4 bg-gray-300">Unique Views</th>
+                <th className="py-2 px-4 bg-gray-300">Clones</th>
+                <th className="py-2 px-4 bg-gray-300">Unique Clones</th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-300">
+              {repos.map(
+                ({
+                  name,
+                  url,
+                  views,
+                  uniqueViews,
+                  clones,
+                  uniqueClones,
+                  yesterdayViews,
+                  yesterdayUniqueViews,
+                  yesterdayClones,
+                  yesterdayUniqueClones,
+                }) => {
+                  return (
+                    <tr key={name} className="border-b border-gray-300">
+                      <td className="py-2 px-4">{name}</td>
+                      <td className="py-2 px-4">
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 hover:underline"
+                        >
+                          {url}
+                        </a>
+                      </td>
+                      <td className="py-2 px-4">
+                        <div className="flex justify-between">
+                          <strong className="text-gray-800">{views}</strong>
+                          <strong className="text-gray-500">{yesterdayViews}</strong>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4">
+                        <div className="flex justify-between">
+                          <strong className="text-gray-800">{uniqueViews}</strong>
+                          <strong className="text-gray-500">{yesterdayUniqueViews}</strong>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4">
+                        <div className="flex justify-between">
+                          <strong className="text-gray-800">{clones}</strong>
+                          <strong className="text-gray-500">{yesterdayClones}</strong>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4">
+                        <div className="flex justify-between">
+                          <strong className="text-gray-800">{uniqueClones}</strong>
+                          <strong className="text-gray-500">{yesterdayUniqueClones}</strong>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
