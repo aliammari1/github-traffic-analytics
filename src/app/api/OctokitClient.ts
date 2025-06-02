cb482a0463c2c4e34f19f3685de948d004028d0c
Ali Ammari <ammari.ali.0001@gmail.com>
2025-06-02 21:41:51 +0100

    Update OctokitClient.ts

--- src/app/api/OctokitClient.ts
+++ src/app/api/OctokitClient.ts
@@ -4,6 +4,9 @@ class OctokitClient {
   private static instance: Octokit | null = null;
 
   public static getInstance(): Octokit {
+if (!process.env.NEXT_PUBLIC_GITHUB_TOKEN) {
+  return new Response(JSON.stringify({ error: "Missing GitHub token" }), { status: 500 });
+}
     if (!OctokitClient.instance) {
       OctokitClient.instance = new Octokit({
         auth: `${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
